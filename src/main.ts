8820ec1136be1d2ce98505593bd7b0db763983b2
Vitória Brancatti <vitoriabrancatti16@gmail.com>
2024-09-25 22:39:21 -0300

    feat: add icon library

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-import './styles.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBell, faMapMarkerAlt, faMap, faUserPlus, faSignInAlt } from '@fortawesome/free-solid-svg-icons'; // Ícones que você vai usar
 
-import App from './App.vue'
-import router from './router'
+import './styles.css';
 
+// Adiciona os ícones à biblioteca do FontAwesome
+library.add(faBell, faMapMarkerAlt, faMap, faUserPlus, faSignInAlt);
 
-const app = createApp(App)
+// Cria a aplicação
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+// Registra o FontAwesome globalmente
+app.component('font-awesome-icon', FontAwesomeIcon);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
