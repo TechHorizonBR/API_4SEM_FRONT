91a0000e72aeda47d942bc96bbf1839f150b07ee
Raul Neto <127263427+raulnt@users.noreply.github.com>
2024-09-26 22:29:02 -0300

    Habilitar botao filtrar (#19)
    * chore: add node_modules in gitignore
    
    * docs: add description to run application
    
    * feat: create Filter component and import in Map component
    
    * feat: create filter and autocomplete component
    
    * feat: import filter in map component
    
    * chore: gitignore
    
    * feat: Device filter initial structure
    
    * feat: Update Device filter initial structure
    
    * feat: Create request method Get Registros
    
    ---------
    
    Co-authored-by: BeatrizPlacido <biaplacsilva0212@gmail.com>
    Co-authored-by: BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
    Co-authored-by: André Meneses <andremeneses0103@gmail.com>

---
+++ src/services/registros.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+class RegistrosService {
+  async getRegistros(id: number) {
+    try {
+      const response = await axios.get(`http://localhost:8080/registros/filtros/2023-01-01/2024-12-31/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao buscar os registros:', error);
+      throw error;
+    }
+  }
+}
+
+export default new RegistrosService();
