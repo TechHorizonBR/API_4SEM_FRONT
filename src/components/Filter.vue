cf7cad303f6c7ae232541c9361909b4883c9cc31
Jhony Santos <123211025+santosjhony12@users.noreply.github.com>
2024-10-09 21:28:42 -0300

    Periods Filter (#43)
    * feat: DarkMode is in progress
    
    * feat: modeDark has been finished in NAV AND FILTERS
    
    * feat: removed mark
    
    * feat: added hover in navbar buttons
    
    * feat: defined periods input has been created but doesn't have action yet.
    
    * feat: working on periods logics
    
    * feat: creating branch
    
    * refactor: changing loading to last spot
    
    * feat: periods filter has been implemented
    
    * style: size filter has ajusted
    
    ---------
    
    Co-authored-by: Vitória Brancatti <vitoriabrancatti16@gmail.com>

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -9,35 +9,19 @@
         :isDark="isDark"
       />
     </div>
-
-    <div class="filter-date" v-if="showDateFilter">
-      <label class="label" for="startDate">Start Date:</label>
-      <input
-        class="input-date"
-        type="date"
-        id="startDate"
-        v-model="startDate"
-      />
-
-      <label class="label" for="endDate">End Date:</label>
-      <input class="input-date" type="date" id="endDate" v-model="endDate" />
-
-      <select class="filter-select" id="deviceInfo" v-model="filter_date">
-        <option value="" selected disabled>Select an option</option>
-        <option value="1">Last 24 hours</option>
-        <option value="7">Last week</option>
-        <option value="30">Last month</option>
-      </select>
-    </div>
+    <DateFilters 
+      :isDark="isDark"
+      @updatePeriod="handleUpdatePeriod" />
 
     <button @click="triggerSearch">Search</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import Autocomplete from "./autocomplete/Autocomplete.vue";
-import DevicesService from "../services/devices";
+  import { ref, onMounted } from 'vue';
+  import Autocomplete from './autocomplete/Autocomplete.vue';
+  import DevicesService from '../services/devices';
+  import DateFilters from '../components/DateFilters.vue';
 
 interface Device {
   fullName: string;
@@ -45,49 +29,60 @@ interface Device {
   userCode: string;
 }
 
-const devices = ref<Device[]>([]);
-const fullName = ref<string>("");
-const codeDevice = ref<string>("");
-const userCode = ref<string>("");
-const showAutocompleteFilter = ref<boolean>(true);
-const showDateFilter = ref<boolean>(false);
-const startDate = ref<string>("");
-const endDate = ref<string>("");
-const emit = defineEmits(["search"]);
-const props = defineProps<{isDark : boolean}>();
 
-const fetchDevices = async () => {
-  try {
-    devices.value = await DevicesService.getDevices();
-  } catch (error) {
-    console.error("Erro ao buscar dispositivos:", error);
-  }
-};
+  const devices = ref<Device[]>([]);
+  const fullName = ref<string>('');
+  const codeDevice = ref<string>('');
+  const userCode = ref<string>('');
+  const showAutocompleteFilter = ref<boolean>(true);
+  const emit = defineEmits(['search']);
+  const props = defineProps<{isDark : boolean}>();
+  const periods = ref<{ dataInicio: string | null, dataFim: string | null }>({
+    dataInicio: null,
+    dataFim: null
+  });
 
-onMounted(() => {
-  fetchDevices();
-});
- 
-const triggerSearch = () => {
+  const handleUpdatePeriod = (period: { dataInicio: string | null; dataFim: string | null }) => {
+    periods.value.dataInicio = period.dataInicio;
+    periods.value.dataFim = period.dataFim;
+  };
+
+  const fetchDevices = async () => {
+    try {
+      devices.value = await DevicesService.getDevices();
+    } catch (error) {
+      console.error('Erro ao buscar dispositivos:', error);
+    }
+  };
+
+  onMounted(() => {
+    fetchDevices();
+  });
+
+  const triggerSearch = () => {
   emit("search", {
     fullName: fullName.value,
     codeDevice: codeDevice.value,
     userCode: userCode.value,
+    dataInicio: periods.value.dataInicio,
+    dataFim: periods.value.dataFim
   });
 };
+
 </script>
 
 <style scoped>
 .filter {
   position: absolute;
-  top: 3vh;
-  left: 3vw;
-  padding: 25px 40px;
-  background-color: #f7f7f7;
-  border-radius: 20px;
-  width: 250px;
-  z-index: 1000;
-  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+    top: 3vh;
+    left: 3vw;
+    padding: 25px 40px;
+    background-color: #f7f7f7;
+    border-radius: 20px;
+    width: 220px;
+    z-index: 1000;
+    box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+  /*  box-shadow: 5px 5px 8px #929292;*/
 }
 
 .label {
@@ -97,29 +92,13 @@ const triggerSearch = () => {
   margin-top: 8px;
 }
 
-.input-date {
-  width: 93%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  font-size: 16px;
-}
-.label {
-  width: 100%;
-  display: block;
-  margin-bottom: 6px;
-  margin-top: 8px;
-  font-size: 20px;
-}
-
-.filter-select {
-  width: 100%;
-  padding: 10px;
-  margin-top: 16px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  font-size: 16px;
-}
+  .label {
+    width: 100%;
+    display: block;
+    margin-bottom: 6px;
+    margin-top: 8px;
+    font-size: 20px;
+  }
 
 button {
   width: 100%;
--- src/services/registros.ts
+++ src/services/registros.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 class RegistrosService {
-  async getRegistros(id: number, page: number) {
+  async getRegistros(id: number, page: number, dataInicio : string, dataFim : string) {
     try {
       const response = await axios.get(
-        `http://localhost:8080/registros/filtros/2023-01-01/2024-12-31/${id}/${page}`,
+        `http://localhost:8080/registros/filtros/${dataInicio}/${dataFim}/${id}/${page}`,
       );
       return response.data;
     } catch (error) {
