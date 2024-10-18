539265e1ae5e0718ad24c206e8a2d846ea247278
santosjhony12 <santosdesouzajhony@gmail.com>
2024-10-18 17:19:26 -0300

    fix: fixing path error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-});
+const routers = [{
+  path: '/',
+  name: 'Map',
+  component: () => import('@/views/MapView.vue')
+}
 
-export default router;
+]
+
+export default routers;
---
+++ src/views/MapView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from "vue";
+import Map from "./components/Map.vue";
+</script>
+
+<template>
+  <Map />
+</template>
