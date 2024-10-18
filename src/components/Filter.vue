48199cc950920ec266a8f7a8cacf4c022f354ad5
santosjhony12 <santosdesouzajhony@gmail.com>
2024-10-18 17:14:03 -0300

    style: fixing some styles css

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,7 +1,6 @@
 <template>
 
   <div class="filter" :style="{backgroundColor: isDark ? '#0a0012e3' : '#f7f7f7cd'}">
-    <!--<h2 :class="isDark ? 'title-filter-dark' : 'title-filter-light'" class="title-filter">LocalTracker</h2>-->
 
     <div class="filter-autocomplete" v-if="showAutocompleteFilter">
       <Autocomplete
@@ -13,7 +12,6 @@
       />
     </div>
 
-    <!-- Campo de Filtro de Período de Datas -->
     <div class="date-range-filter">
       <label for="date-range" class="label-position-time" :style="{color: isDark ? 'white' : 'black'}">Position Time Range:</label>
       <input :class="isDark ? 'input-data-dark' : 'input-data-light'"
@@ -25,15 +23,14 @@
       />
     </div>
 
-
     <DateFilters 
       :isDark="isDark"
       @updatePeriod="handleUpdatePeriod" />
 
     <button @click="triggerSearch">Search</button>
 
-    <div class="selected-users">
-      <h3 v-if="selectedUsers.length !== 0" :class=" isDark ? 'labelDark' : 'labelLight'">
+    <div class="selected-users" v-if="selectedUsers.length !== 0">
+      <h3 :class=" isDark ? 'labelDark' : 'labelLight'">
         Selected Users
       </h3>
       <div class="users-scrool">
@@ -45,21 +42,22 @@
       </div>
     </div>
 
-    <p v-if="showMessage">
-      Usuário já selecionado
+    <p v-if="showMessage" :style="{color: isDark ? 'white' : 'black'}">
+      {{ message }}
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
   import { ref, onMounted, nextTick } from 'vue';
-  import flatpickr from 'flatpickr'; // Biblioteca para seleção de data
-  import 'flatpickr/dist/flatpickr.css'; // Estilo para flatpickr
+  import flatpickr from 'flatpickr';
+  import 'flatpickr/dist/flatpickr.css';
   import Autocomplete from './autocomplete/Autocomplete.vue';
   import DevicesService from '../services/devices';
   import DateFilters from '../components/DateFilters.vue';
   import SelectedUser from './SelectedUser.vue';
 
+  // VARIAVEIS
   interface Device {
     fullName: string;
     codeDevice: string;
@@ -82,56 +80,60 @@
     cicleColor: string
   }>>([]);
   const showMessage = ref<boolean>(false);
-
+  const message = ref<string>('');
   const dateRangePicker = ref<HTMLInputElement | null>(null);
-
-  // Variável de controle da data selecionada
   const selectedDate = ref<string | null>(null);
 
-const fetchDevices = async () => {
-  try {
-    devices.value = await DevicesService.getDevices();
-  } catch (error) {
-    console.error("Erro ao buscar dispositivos:", error);
-  }
-};
-const handleDateRangeChange = (selectedDates: any) => {
-  if (selectedDates.length === 2) {
-    periods.value.dataInicio = selectedDates[0].toISOString().split('T')[0];
-    periods.value.dataFim = selectedDates[1].toISOString().split('T')[0];
-    console.log("Data de Início:", periods.value.dataInicio);
-    console.log("Data de Fim:", periods.value.dataFim);
-  }
-};
-
-const updateFlatpickrDates = () => {
-  if (dateRangePicker.value) {
-    const picker = flatpickr(dateRangePicker.value);
-    const startDate = periods.value.dataInicio ? new Date(periods.value.dataInicio) : null;
-    const endDate = periods.value.dataFim ? new Date(periods.value.dataFim) : null;
-
-    // Definir o intervalo de datas no flatpickr
-    if (startDate && endDate) {
-      picker.setDate([startDate, endDate]);
+  // MÉTODOS
+  const fetchDevices = async () => {
+    try {
+      devices.value = await DevicesService.getDevices();
+    } catch (error) {
+      console.error("Erro ao buscar dispositivos:", error);
     }
-  }
-};
+  };
 
+  const handleDateRangeChange = (selectedDates: any) => {
+    if (selectedDates.length === 2) {
+      periods.value.dataInicio = selectedDates[0].toISOString().split('T')[0];
+      periods.value.dataFim = selectedDates[1].toISOString().split('T')[0];
+      console.log("Data de Início:", periods.value.dataInicio);
+      console.log("Data de Fim:", periods.value.dataFim);
+    }
+  };
 
-onMounted(async () => {
-  await fetchDevices();
-  
-  // Inicializando flatpickr para intervalo de datas
-  nextTick(() => {
+  const updateFlatpickrDates = () => {
     if (dateRangePicker.value) {
-      flatpickr(dateRangePicker.value, {
+      const picker = flatpickr(dateRangePicker.value, {
         mode: "range",
         dateFormat: "Y-m-d",
         onChange: handleDateRangeChange
       });
+
+      const startDate = periods.value.dataInicio ? new Date(periods.value.dataInicio) : null;
+      const endDate = periods.value.dataFim ? new Date(periods.value.dataFim) : null;
+
+      if (startDate && endDate) {
+        picker.setDate([startDate, endDate], true);
+      } else {
+        picker.clear();
+      }
     }
+  };
+
+  onMounted(async () => {
+    await fetchDevices();
+    
+    nextTick(() => {
+      if (dateRangePicker.value) {
+        flatpickr(dateRangePicker.value, {
+          mode: "range",
+          dateFormat: "Y-m-d",
+          onChange: handleDateRangeChange
+        });
+      }
+    });
   });
-});
 
   const handleUpdatePeriod = (period: { dataInicio: string | null; dataFim: string | null }) => {
     periods.value.dataInicio = period.dataInicio;
@@ -140,46 +142,63 @@ onMounted(async () => {
   };
 
   const triggerSearch = () => {
-
-  if(selectedUsers.value.some(user => user.nameUser === fullName.value)){
+    if(fullName.value === null || periods.value.dataFim === null || periods.value.dataInicio === null || userCode.value === null){
+      message.value = 'It is needed all fields are completed!'
       showMessage.value = true;
       setTimeout(() => {
-          showMessage.value = false;
+        showMessage.value = false;
       }, 3000);
-  }else{
-    const color = generateRandomColor()
-    selectedUsers.value.push({
-      nameUser: fullName.value,
-      cicleColor: color
-    });
-    emit("search", {
-      fullName: fullName.value,
-      codeDevice: codeDevice.value,
-      userCode: userCode.value,
-      dataInicio: periods.value.dataInicio,
-      dataFim: periods.value.dataFim,
-      selectedDate: selectedDate.value, // Data selecionada incluída na pesquisa
-      cicleColor: color
-    });
-  }  
+    }else{
+      if(selectedUsers.value.some(user => user.nameUser === fullName.value)){
+        message.value = 'Usuário already has selected!';
+        showMessage.value = true;
+        setTimeout(() => {
+            showMessage.value = false;
+        }, 3000);
+      }else{
+        const color = generateRandomColor()
+        selectedUsers.value.push({
+          nameUser: fullName.value,
+          cicleColor: color
+        });
+        emit("search", {
+          fullName: fullName.value,
+          codeDevice: codeDevice.value,
+          userCode: userCode.value,
+          dataInicio: periods.value.dataInicio,
+          dataFim: periods.value.dataFim,
+          selectedDate: selectedDate.value,
+          cicleColor: color
+        });
+        fullName.value = '';
+        codeDevice.value = '';
+        userCode.value = '';
+        periods.value.dataInicio = null;
+        periods.value.dataFim = null;
+
+        if (dateRangePicker.value) {
+          const picker = flatpickr(dateRangePicker.value);
+          picker.clear();
+        }
+      }  
+    }
+    
+
 };
 
-  const handleRemoveUser = (username: string) => {
-    const index = selectedUsers.value.findIndex(user => user.nameUser === username);
-
-    if(index !== -1){
-      selectedUsers.value.splice(index, 1);
-    }
+const handleRemoveUser = (username: string) => {
+  const index = selectedUsers.value.findIndex(user => user.nameUser === username);
+  if(index !== -1){
+    selectedUsers.value.splice(index, 1);
   }
-  const generateRandomColor = () => {
-    const red = Math.floor(Math.random() * 56 + 200).toString(16);  // Valores altos para vermelho
-  const green = Math.floor(Math.random() * 56 + 200).toString(16); // Valores altos para verde
-  const blue = Math.floor(Math.random() * 56 + 200).toString(16);  // Valores altos para azul
+}
 
+const generateRandomColor = () => {
+  const red = Math.floor(Math.random() * 56 + 200).toString(16);
+  const green = Math.floor(Math.random() * 56 + 200).toString(16);
+  const blue = Math.floor(Math.random() * 56 + 200).toString(16);
   return `#${red.padStart(2, '0')}${green.padStart(2, '0')}${blue.padStart(2, '0')}`;
- }
- 
-
+}
 </script>
 
 <style scoped>
@@ -193,26 +212,21 @@ onMounted(async () => {
   z-index: 1000;
   box-shadow: 0 1px 2px rgba(60, 64, 67, 0.3), 0 2px 6px 2px rgba(60, 64, 67, 0.15);
 }
-
 .date-range-filter {
   margin-bottom: 15px;
 }
-
 .date-range-filter input {
   width: 93%;
   padding: 10px;
   border-radius: 8px;
   font-size: 14px;
 }
-
-
 .label {
   width: 100%;
   display: block;
   margin-bottom: 6px;
   margin-top: 8px;
 }
-
 .label {
   width: 100%;
   display: block;
@@ -220,11 +234,9 @@ onMounted(async () => {
   margin-top: 8px;
   font-size: 20px;
 }
-
 .label-position-time{
   font-size: 1.2em;
 }
-
 button {
   width: 100%;
   background-color: #35005d;
@@ -235,19 +247,15 @@ button {
   border-radius: 8px;
   cursor: pointer;
 }
-
 button:hover {
   background-color: #3c0564;
 }
-
 .fade-enter-active {
   animation: fadeInUp 0.3s ease-out;
 }
-
 .fade-leave-active {
   animation: fadeOutDown 0.3s ease-in forwards;
 }
-
 @keyframes fadeInUp {
   from {
     opacity: 0;
@@ -258,7 +266,6 @@ button:hover {
     transform: translateY(0);
   }
 }
-
 @keyframes fadeOutDown {
   from {
     opacity: 1;
@@ -285,19 +292,15 @@ button:hover {
   color: black;
   border: 1px solid rgb(156, 156, 156);
 }
-
 h3{
   font-weight: normal;
 }
 .selected-users{
   margin: 15% 0 0 0;
 }
-
-/* Estilo para o campo de data */
 .date-filter {
   margin-bottom: 15px;
 }
-
 .date-filter input {
   width: 93%;
   padding: 10px;
@@ -305,22 +308,17 @@ h3{
   border: 1px solid rgb(156, 156, 156);
   font-size: 14px;
 }
-
 .users-scrool{
   overflow-y: auto;
   max-height: 25vh;
 }
-
 .users-scrool::-webkit-scrollbar {
-  width: 10px; /* Define a largura da barra de rolagem */
+  width: 10px;
 }
-
-/* Estilo da parte "ativa" da barra de rolagem (thumb) */
 .users-scrool::-webkit-scrollbar-thumb {
-  background-color: #35005d; /* Cor escura */
-  border-radius: 10px; /* Bordas arredondadas para um visual mais suave */
+  background-color: #35005d;
+  border-radius: 10px;
 }
-
 .users-scrool::-webkit-scrollbar-track {
   background-color: #f1f1f1;
 }
