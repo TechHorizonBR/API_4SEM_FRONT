25672a65305be958f9fbfda378d8586a25f3abae
AndreMeneses0103 <andremeneses0103@gmail.com>
2024-10-30 20:20:16 -0300

    refactor: Types added to map script

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,19 +1,28 @@
 <template>
-    <div :class="{
-        'dark-controls': mapModeStore.isDarkMode,
-        'light-controls': !mapModeStore.isDarkMode,
-    }" class="map-wrap">
+    <div
+        :class="{
+            'dark-controls': mapModeStore.isDarkMode,
+            'light-controls': !mapModeStore.isDarkMode,
+        }"
+        class="map-wrap"
+    >
         <div class="map" ref="mapContainer">
             <div id="buttonConfig">
                 <LightDarkToggle />
             </div>
-            <img v-if="loading" src="/loading.gif" id="loading" alt="Loading..." />
+            <img
+                v-if="loading"
+                src="/loading.gif"
+                id="loading"
+                alt="Loading..."
+            />
 
-            <Nav @toggleFilter="toggleFilter" :isDark="mapModeStore.isDarkMode" />
+            <Nav
+                @toggleFilter="toggleFilter"
+                :isDark="mapModeStore.isDarkMode"
+            />
 
-            <transition
-                name="fade"
-            >
+            <transition name="fade">
                 <Filter
                     v-show="showFilter"
                     @search="handleSearch"
@@ -28,7 +37,7 @@
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { Map, MapStyle, config, Marker } from "@maptiler/sdk";
 import { shallowRef, onMounted, onUnmounted, markRaw, watch, ref } from "vue";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
@@ -37,30 +46,37 @@ import Filter from "./Filter.vue";
 import RegistrosService from "../services/registros";
 import Nav from "./Nav.vue";
 import { useMapModeStore } from "@/stores/useMapMode";
-import DrawPolygon from './DrawPolygon.vue';
+import DrawPolygon from "./DrawPolygon.vue";
 
-
+interface SearchParams {
+    fullName: string;
+    codeDevice: string;
+    userCode: number;
+    dataInicio: string;
+    dataFim: string;
+    selectedDate: string;
+    cicleColor: string;
+}
 
 const mapContainer = shallowRef(null);
-const map = shallowRef(null);
+const map = shallowRef<Map | null>(null);
 const mapModeStore = useMapModeStore();
-let dados;
-let allCoords = [];
-const all_markers = shallowRef([]);
+const allCoords = ref<any[]>([]);
+const all_markers = shallowRef<Marker[][]>([]);
 const loading = shallowRef(false);
 const showFilter = shallowRef(true);
 const actualUser = ref(0);
-const messageEmpty =shallowRef('');
-const showMessageEmpty = shallowRef(false)
+const messageEmpty = shallowRef("");
+const showMessageEmpty = shallowRef(false);
 
 onMounted(() => {
     config.apiKey = "tF1lf7jSig6Ou8IuaLtw";
     inicializarMapa();
 
     const controlElements = document.getElementsByClassName(
-        "maplibregl-ctrl-top-right",
+        "maplibregl-ctrl-top-right"
     );
-    for (let element of controlElements) {
+    for (let element of Array.from(controlElements) as HTMLElement[]) {
         element.style.top = "auto";
         element.style.bottom = "80px";
         element.style.right = "10px";
@@ -75,22 +91,22 @@ const changeLoading = () => {
     loading.value = !loading.value;
 };
 
-const handleSearch = (searchParams) => {
+const handleSearch = (searchParams: SearchParams) => {
     changeLoading();
-    getPoints(searchParams)
+    getPoints(searchParams);
 };
 
-const handleDelete = (deleteParams, idUser) => {
+const handleDelete = (deleteParams: number, idUser: number) => {
     all_markers.value[deleteParams].forEach((marker) => marker.remove());
     all_markers.value.splice(deleteParams, 1);
 
     const routeId = `route${idUser}`;
 
-    if (map.value.getLayer(routeId)) {
+    if (map.value?.getLayer(routeId)) {
         map.value.removeLayer(routeId);
     }
 
-    if (map.value.getSource(routeId)) {
+    if (map.value?.getSource(routeId)) {
         map.value.removeSource(routeId);
     }
 
@@ -101,46 +117,63 @@ const handleDelete = (deleteParams, idUser) => {
     }
 };
 
-
-const getPoints = async (searchParams) => {
+const getPoints = async (searchParams: SearchParams) => {
     try {
-        const firstReq = await RegistrosService.getRegistros(searchParams.userCode, 0, searchParams.dataInicio, searchParams.dataFim);
-        console.log(firstReq)
+        const firstReq: any = await RegistrosService.getRegistros(
+            searchParams.userCode,
+            0,
+            searchParams.dataInicio,
+            searchParams.dataFim
+        );
         if (firstReq.registers.length != 0) {
             const allPages = firstReq.totalPages;
-            allCoords.push(firstReq.registers);
+            allCoords.value.push(firstReq.registers);
             transformData(firstReq.registers, 0, allPages, searchParams);
-            map.value.fitBounds([
-                [firstReq.maxMinCoordinates.minLongitude - 1, firstReq.maxMinCoordinates.minLatitude - 1],
-                [firstReq.maxMinCoordinates.maxLongitude + 1, firstReq.maxMinCoordinates.maxLatitude + 1]
+            map.value?.fitBounds([
+                [
+                    firstReq.maxMinCoordinates.minLongitude - 1,
+                    firstReq.maxMinCoordinates.minLatitude - 1,
+                ],
+                [
+                    firstReq.maxMinCoordinates.maxLongitude + 1,
+                    firstReq.maxMinCoordinates.maxLatitude + 1,
+                ],
             ]);
 
             for (let page = 1; page <= allPages; page++) {
-                const req = await RegistrosService.getRegistros(searchParams.userCode, page, searchParams.dataInicio, searchParams.dataFim);
+                const req = await RegistrosService.getRegistros(
+                    searchParams.userCode,
+                    page,
+                    searchParams.dataInicio,
+                    searchParams.dataFim
+                );
                 if (req) {
-                    allCoords.push(req.registers);
+                    allCoords.value.push(req.registers);
                     transformData(req.registers, page, allPages, searchParams);
                 }
             }
-        }else{
+        } else {
             changeLoading();
-            messageEmpty.value = 'This user has not registers in this period.'
-            showMessageEmpty.value = true
-            setTimeout(() =>{
-                showMessageEmpty.value = false
-                messageEmpty.value = ''
+            messageEmpty.value = "This user has not registers in this period.";
+            showMessageEmpty.value = true;
+            setTimeout(() => {
+                showMessageEmpty.value = false;
+                messageEmpty.value = "";
             }, 3000);
-
         }
     } catch (error) {
         console.error("Error:", error);
     }
 };
 
-function transformData(data, page, totalpages, elementData) {
+function transformData(
+    data: any,
+    page: number,
+    totalpages: number,
+    elementData: any
+) {
     if (data) {
-        dados = data;
-        plotPontos(dados, page, totalpages, elementData);
+        plotPontos(data, page, totalpages, elementData);
     }
 }
 
@@ -153,70 +186,98 @@ onUnmounted(() => {
 function inicializarMapa() {
     const initialState = { lng: -60.6714, lat: 2.81954, zoom: 1 };
 
-    map.value = markRaw(
-        new Map({
-            container: mapContainer.value,
-            style: mapModeStore.isDarkMode ? MapStyle.STREETS.DARK : MapStyle.STREETS,
-            center: [initialState.lng, initialState.lat],
-            zoom: initialState.zoom,
-        }),
-    );    
+    if (mapContainer.value) {
+        map.value = markRaw(
+            new Map({
+                container: mapContainer.value,
+                style: mapModeStore.isDarkMode
+                    ? MapStyle.STREETS.DARK
+                    : MapStyle.STREETS,
+                center: [initialState.lng, initialState.lat],
+                zoom: initialState.zoom,
+            })
+        );
+    } else {
+        console.warn("mapContainer is not available");
+    }
 }
 
-async function plotPontos(allPoints, page, totalpages, elementData) {
+async function plotPontos(
+    allPoints: any,
+    page: number,
+    totalpages: number,
+    elementData: any
+) {
     if (!all_markers.value[actualUser.value]) {
         all_markers.value[actualUser.value] = [];
     }
     const fin = allPoints.length - 1;
-    const [firstName, secondName] = elementData.fullName.split(' ');
+    const [firstName, secondName] = elementData.fullName.split(" ");
     const iniciais = firstName[0] + secondName[0];
 
     let el_start = createMarkerElement(
         elementData.fullName,
-        createPin(elementData.cicleColor, "START")
+        createPin(elementData.cicleColor, "START", false)
     );
 
     let el_finish = createMarkerElement(
         elementData.fullName,
-        createPin(elementData.cicleColor, "FINISH")
+        createPin(elementData.cicleColor, "FINISH", false)
     );
 
-    allPoints.forEach((point, index) => {
-        let el_point = createMarkerElement(
-            elementData.fullName,
-            createPin(elementData.cicleColor, iniciais, point.isStopped)
-        );
-        const defaultMark = new Marker({ element: el_point })
-            .setLngLat([point.longitude, point.latitude])
-            .addTo(map.value);
-        all_markers.value[actualUser.value].push(defaultMark);
+    allPoints.forEach((point: any, index: number) => {
+        if (point.isStopped) {
+            let el_point = createMarkerElement(
+                elementData.fullName,
+                createPin(elementData.cicleColor, iniciais, point.isStopped)
+            );
+
+            if (map.value) {
+                const defaultMark = new Marker({ element: el_point })
+                    .setLngLat([point.longitude, point.latitude])
+                    .addTo(map.value);
+                all_markers.value[actualUser.value].push(defaultMark);
+            } else {
+                console.warn("Map is not initialized");
+            }
+        }
     });
 
     if (page === 0) {
-        let startMark = new Marker({ element: el_start })
-            .setLngLat([allPoints[0].longitude, allPoints[0].latitude])
-            .addTo(map.value);
-        all_markers.value[actualUser.value].push(startMark);
+        if (map.value) {
+            let startMark = new Marker({ element: el_start })
+                .setLngLat([allPoints[0].longitude, allPoints[0].latitude])
+                .addTo(map.value);
+            all_markers.value[actualUser.value].push(startMark);
+        } else {
+            console.warn("Map not initialized");
+        }
     }
     if (!allPoints || allPoints.length === 0) {
         return;
     }
 
     if (page === totalpages - 1) {
-        let finishMark = new Marker({ element: el_finish })
-            .setLngLat([allPoints[fin].longitude, allPoints[fin].latitude])
-            .addTo(map.value);
+        if (map.value) {
+            let finishMark = new Marker({ element: el_finish })
+                .setLngLat([allPoints[fin].longitude, allPoints[fin].latitude])
+                .addTo(map.value);
 
-        all_markers.value[actualUser.value].push(finishMark);
+            all_markers.value[actualUser.value].push(finishMark);
+        }
 
-        let flattenedCoords = allCoords.flat();
-        let formattedCoords = flattenedCoords.map(coord => [coord.longitude, coord.latitude]);
+        let flattenedCoords = allCoords.value.flat();
+        let formattedCoords: any[][] | null = flattenedCoords.map((coord) => [
+            coord.longitude,
+            coord.latitude,
+        ]);
 
         const routeId = `route${elementData.userCode}`;
-        map.value.addSource(routeId, {
+        map.value?.addSource(routeId, {
             type: "geojson",
             data: {
                 type: "Feature",
+                properties: {},
                 geometry: {
                     type: "LineString",
                     coordinates: formattedCoords,
@@ -224,7 +285,7 @@ async function plotPontos(allPoints, page, totalpages, elementData) {
             },
         });
 
-        map.value.addLayer({
+        map.value?.addLayer({
             id: routeId,
             type: "line",
             source: routeId,
@@ -233,26 +294,38 @@ async function plotPontos(allPoints, page, totalpages, elementData) {
                 "line-cap": "round",
             },
             paint: {
-                "line-color": elementData.cicleColor,
+                "line-color": "#000",
                 "line-width": 2,
                 "line-dasharray": [1, 1],
             },
         });
 
         formattedCoords = null;
-        allCoords = [];
+        allCoords.value = [];
 
         actualUser.value++;
         changeLoading();
     }
-
-
 }
 
+// function walkPoints(allPoints) {
+//     allPoints.forEach((point, index) => {
+//         let el_point = createMarkerElement(
+//             elementData.fullName,
+//             createPin("#000", ":)", false)
+//         );
+//         const defaultMark = new Marker({ element: el_point })
+//             .setLngLat([point.longitude, point.latitude])
+//             .addTo(map.value);
+//         all_markers.value[actualUser.value].push(defaultMark);
+//         setTimeout(()=>{
 
-function createPin(color, name, isStopped) {
+//         },1000);
+//     });
+// }
+
+function createPin(color: string, name: string, isStopped: boolean) {
     let user_pin = document.createElement("div");
-
 
     if (name == "START" || name == "FINISH") {
         user_pin.style.borderRadius = "3px";
@@ -264,12 +337,13 @@ function createPin(color, name, isStopped) {
         user_pin.style.borderRadius = "50%";
         user_pin.style.height = `25px`;
     }
-    if(isStopped){
-      user_pin.style.border = "dotted 2px"
+    if (isStopped) {
+        user_pin.style.border = "dotted 2px";
     }
 
     user_pin.style.minWidth = `25px`;
-    user_pin.style.boxShadow = "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);"
+    user_pin.style.boxShadow =
+        "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);";
     user_pin.style.backgroundColor = color;
     user_pin.style.color = "black";
     user_pin.style.display = "flex";
@@ -280,7 +354,7 @@ function createPin(color, name, isStopped) {
     return user_pin;
 }
 
-function createImg(imgSrc) {
+function createImg(imgSrc: string) {
     let img = document.createElement("img");
     img.src = imgSrc;
     img.style.width = `25px`;
@@ -290,11 +364,10 @@ function createImg(imgSrc) {
     return img;
 }
 
-function createMarkerElement(name, element) {
+function createMarkerElement(name: string, element: HTMLElement) {
     let el = document.createElement("div");
 
     let son = document.createElement("div");
-
 
     son.textContent = `${name}`;
     son.style.backgroundColor = "#FFF";
@@ -324,11 +397,13 @@ watch(
     () => mapModeStore.isDarkMode,
     (isDarkMode) => {
         if (map.value) {
-            map.value.setStyle(isDarkMode ? MapStyle.STREETS.DARK : MapStyle.STREETS);
+            map.value.setStyle(
+                isDarkMode ? MapStyle.STREETS.DARK : MapStyle.STREETS
+            );
             map.value.on("load", () => {
                 const attributionControl = document.querySelector(
-                    ".maplibregl-ctrl-attrib a",
-                );
+                    ".maplibregl-ctrl-attrib a"
+                ) as HTMLElement;
                 if (attributionControl) {
                     attributionControl.style.display = "none";
                 }
@@ -337,27 +412,29 @@ watch(
             const controlElements =
                 document.getElementsByClassName("maplibregl-ctrl");
             for (let element of controlElements) {
+                const htmlElement = element as HTMLElement; // Asserção de tipo para HTMLElement
                 if (isDarkMode) {
-                    element.style.backgroundColor = "#0a0012e3";
-                    element.style.color = "#fff";
+                    htmlElement.style.backgroundColor = "#0a0012e3";
+                    htmlElement.style.color = "#fff";
                 } else {
-                    element.style.backgroundColor = "#fff";
-                    element.style.color = "#000";
+                    htmlElement.style.backgroundColor = "#fff";
+                    htmlElement.style.color = "#000";
                 }
             }
 
             const controlIcons = document.getElementsByClassName(
-                "maplibregl-ctrl-icon",
+                "maplibregl-ctrl-icon"
             );
             for (let icon of controlIcons) {
+                const iconHtml = icon as HTMLElement;
                 if (isDarkMode) {
-                    icon.style.filter = "invert(100%)";
+                    iconHtml.style.filter = "invert(100%)";
                 } else {
-                    icon.style.filter = "none";
+                    iconHtml.style.filter = "none";
                 }
             }
         }
-    },
+    }
 );
 </script>
 
