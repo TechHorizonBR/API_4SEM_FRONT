f4c2ed22449311ba34fae959e03bfdb6a9c85b1a
amandavannuccic <vannucci_job@hotmail.com>
2024-11-21 22:34:46 -0300

    feat: create method getAllUsers

--- src/services/registros.ts
+++ src/services/registros.ts
@@ -39,6 +39,21 @@ class RegistrosService {
       console.log("Error to find user:", error);
     }
   }
+
+  async getAllUsers(){
+    const router = useRouter();
+    try{
+      const response = await apiClient.get('/usersys');
+      return response.data;
+    }catch( error:any){
+      if(error.status === 401){
+        alert("Session expired! Please log in again.")
+        router.push({path:'/'});
+      }
+      console.log("Error to find user:", error);
+    }
+  }
+
 }
 
 export default new RegistrosService();
