45c0198715a904c8afb96e220d6fc156b7bace95
AndreMeneses0103 <andremeneses0103@gmail.com>
2024-11-21 21:45:17 -0300

    Request by username in development

--- src/services/registros.ts
+++ src/services/registros.ts
@@ -21,6 +21,24 @@ class RegistrosService {
     }
     }
   }
+
+  async getUserByName(
+    name:string
+  ){
+    const router = useRouter();
+    try{
+      const response = await apiClient.get(
+        `/usersys/username?username=${name}`,
+      );
+      return response.data;
+    }catch(error: any){
+      if(error.status === 401){
+        alert("Session expired! Please log in again.");
+        router.push({path: '/'});
+      }
+      console.log("Error to find user:", error);
+    }
+  }
 }
 
 export default new RegistrosService();
