21d4f84871ea430e7a06cc6a65f84f7ced7d860c
Kainanthyz <santos.blanzgamer@gmail.com>
2024-11-28 13:14:14 -0300

    style: update with the new table style

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -1,211 +1,267 @@
 <template>
   <div class="container">
-    <div class="box" :class="{'containerDark':isDark, 'containerLight':!isDark}">
+    <div class="box" :class="{ 'containerDark': isDark, 'containerLight': !isDark }">
       <h1 class="title">User System Manager</h1>
       <div class="block0">
-
-        <div class="block1">
-          <button @click="findUser" class="sidebar-button">Find By Username</button>
-          <button @click="createUser" class="sidebar-button">Create User</button>
-          <button @click="updateUser" class="sidebar-button">Update User</button>
-          <button @click="closeAddUser" class="sidebar-button" id="bClose">Close</button>
+        <div class="block1" :class="{ 'block1Dark': isDark, 'block1Light': !isDark }">
+          <a class="sidebar-button" @click="toggleIsVisibleFindUser">+ Find By Username</a>
+          <a class="sidebar-button" @click="toggleIsVisibleCreateUser">+ Create User</a>
+          <a class="sidebar-button" @click="toggleIsVisibleAllUsers">+ See all users</a>
+          <a class="sidebar-button" @click="closeAddUser"  id="bClose">+ Close</a>
         </div>
 
         <div class="block2">
-          <div class="blockForm">
-            <div class="fline1" :class="{'blockDark':isDark, 'blockLight':!isDark}">
-              <div class="case1">
-                <label for="username">Username:</label>
-                <select id="username" v-model="usuario">
-                  <option disabled value="">Selecione um usuário</option>
-                  <option v-for="usuario in usuarios" :key="usuario.id" :value="usuario">
-                    {{ usuario.name }}
-                  </option>
-                </select>
-              </div>
-              
-              <div class="case2">
-                <label for="role">Role:</label>
-                <select :class="{'usernameDark':isDark, 'usernameLight':!isDark}" id="role" v-model="user.role">
-                  <option disabled value="">Select one role</option>
-                  <option value="ROLE_ADMIN">Admin</option>
-                  <option value="1">User</option>
-                </select >
-              </div>
-            </div>
-
-            <div class="fline2">
-              <div class="case3">
-                <label for="password">Password:</label>
-                <input type="password" id="password" :class="{'usernameDark':isDark, 'usernameLight':!isDark}" placeholder="Type the password" />
-              </div>
-              <div class="case3">
-                <label for="confirm-password">Confirm the Password:</label>
-                <input type="password" id="confirm-password" :class="{'usernameDark':isDark, 'usernameLight':!isDark}" placeholder="Confirm the password" />
-              </div>
-            </div>
-          </div>
+          <!-- Tabela de Usuários -->
           <div class="table-container">
-          <table :class="{'tableDark':isDark, 'tableLight':!isDark}">
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Password</th>
-                <th>Role</th>
-                <th>CreatedAt</th>
-                <th>ModifiedAt</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="user in users" :key="user.username">
-                <td>{{ user.username }}</td>
-                <td>{{ user.password }}</td>
-                <td>{{ user.role }}</td>
-                <td>{{ user.createdAt }}</td>
-                <td>{{ user.modifiedAt }}</td>
-                <td>
-                  <button @click="updateUser(usuario)">Atualizar</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            <table :class="{ 'tableDark': isDark, 'tableLight': !isDark }">
+              <thead>
+                <tr>
+                  <th>Username</th>
+                  <th>Password</th>
+                  <th>Role</th>
+                  <th>CreatedAt</th>
+                  <th>ModifiedAt</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(usuario, index) in usuarios" :key="usuario.id">
+                  <td>
+                    <input 
+                      v-if="usuario.isEditing" 
+                      v-model="usuario.name" 
+                      :class="{ 'usernameDark': isDark, 'usernameLight': !isDark }"
+                    />
+                    <span v-else>{{ usuario.name }}</span>
+                  </td>
+                  <td>
+                    <input 
+                      v-if="usuario.isEditing" 
+                      v-model="usuario.username" 
+                      :class="{ 'usernameDark': isDark, 'usernameLight': !isDark }"
+                    />
+                    <span v-else>{{ usuario.username }}</span>
+                  </td>
+                  <td>
+                    <input 
+                      v-if="usuario.isEditing" 
+                      v-model="usuario.password" 
+                      :class="{ 'usernameDark': isDark, 'usernameLight': !isDark }" 
+                      type="password"
+                    />
+                    <span v-else>{{ usuario.password }}</span>
+                  </td>
+                  <td>
+                    <select 
+                      v-if="usuario.isEditing" 
+                      v-model="usuario.role" 
+                      :class="{ 'usernameDark': isDark, 'usernameLight': !isDark }">
+                      <option disabled selected>Select one role</option>
+                      <option value="ROLE_ADMIN">Admin</option>
+                      <option value="1">User</option>
+                    </select>
+                    <span v-else>{{ usuario.role }}</span>
+                  </td>
+                  <td>{{ usuario.createdAt }}</td>
+                  <td>{{ usuario.modifiedAt }}</td>
+                  <td>
+                    <button 
+                      v-if="usuario.isEditing" 
+                      @click="saveUser(usuario)" 
+                      class="sidebar-button">Save</button>
+                    <button 
+                      v-else 
+                      @click="editUser(usuario)" 
+                      class="sidebar-button">Edit</button>
+                    <button 
+                      @click="removeUser(usuario)" 
+                      class="sidebar-button">Remove</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-  import apiClient from '@/services/axiosConfig'; 
-  import RegistroService from '@/services/registros';
-  import { userStore } from '@/stores/token';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import registros from '@/services/registros';
+import apiClient from '@/services/axiosConfig';
+import { userStore } from '@/stores/token';
+import { off } from 'process';
 
-export default {
-  data() {
-    return {
-      usuarios: [],
-      usuario: "",
-      user: {
-        id:'',
-        username: '',
-        password: '',
-        role: "",
-      },
-      confirmPassword: '',
-      users: [] // Lista de usuários
-    };
-  },
-  methods: {
 
-    async getAllUsers() {
-      try {
-        const todosUsuarios = await RegistroService.getAllUsers();
-        this.usuarios = todosUsuarios;
-        console.log(this.usuarios);
-      } catch (error) {
-        console.error(error);
-      }
-    },
+  const isVisibleFindByUser = ref<boolean>(false);
+  const isVisibleCreateUser = ref<boolean>(false);
+  const usuarios = ref<any[]>([]);
 
-    findUser() {
-      // Função para buscar usuário
-    },
-    createUser() {
-      // Função para criar usuário
-    },
-    async updateUser() {
+  const toggleIsVisibleCreateUser = () => {
+    isVisibleFindByUser.value = false;
+    isVisibleCreateUser.value = true;
+  };
 
-      
-      if (this.user.password !== this.confirmPassword) {
-        alert("Passwords do not match!");
-        return;
-      }
-      
+  const toggleIsVisibleFindUser = () => {
+    isVisibleCreateUser.value = false;
+    isVisibleFindByUser.value = true;
+  };
 
-      const userId = userStore().user.id;
-
-      // Dados a serem enviados para o backend
-      const userUpdateData = {
-        id: userId,
-        username: this.user.username,
-        password: this.user.password,
-        role: this.user.role
-      };
-
-      const userData={
-        id: this.usuario.id,
-        name: this.usuario.name,
-        role: this.user.role
-      }
-
-      apiClient.put(`/usersys/update-user?id=${userUpdateData.id}`, userData)
-        .then(response => {
-          const updatedUser = response.data;
-
-          // Atualiza a lista de usuários no frontend
-          const index = this.users.findIndex(user => user.username === updatedUser.username);
-          if (index !== -1) {
-            this.users[index] = updatedUser;
-          }
-
-          // Limpa o formulário após a atualização
-          this.user = { id:null, username: "", password: "", role: "" };
-          this.confirmPassword = "";
-
-          alert("User updated successfully!");
-        })
-        .catch(error => {
-          console.error("Error updating user:", error);
-          alert("Failed to update user.");
-        });
-    },
-    closeAddUser() {
+  const toggleIsVisibleAllUsers = () => {
+    isVisibleCreateUser.value = false;
+    isVisibleFindByUser.value = false;
+  };
+  
+  const closeAddUser= () => {
       // Função para fechar a adição de usuário
+    };
+  const userId = userStore().user.id;
+
+  const userUpdateData = {
+      id: userId
+  };
+
+
+  const getAllUsersSys = async () => {
+    try {
+      const todosUsuarios = await registros.getAllUsers();
+      usuarios.value = todosUsuarios.map((usuario: any) => ({
+        ...usuario,
+        isEditing: false,  // Adicionando isEditing a todos os usuários ao carregar
+      }));
+    } catch (error) {
+      console.error(error);
     }
-  },
-  mounted(){
-    const getUsuarios = async() =>{
-      try{
-        const allUsers = await RegistroService.getAllUsers();
-        console.log("USUARIOS:", allUsers);
-      }catch(error){
-        console.error("Erro:", error);
+  };
+
+  const editUser = (usuario: any) => {
+    usuario.isEditing = true; // Ativar modo de edição
+  };
+
+  const saveUser = async (usuario:any) => {
+
+    
+    // Dados a serem enviados para o backend
+    
+    try {
+      // Envia os dados atualizados do usuário para a API
+      const response = await apiClient.put(`/usersys/update-user?id=${userUpdateData.id}`, {
+        id:usuario.id,
+        name:usuario.name,
+        role: usuario.role
+      });
+
+      if (response.status === 200 || response.status === 204) {
+        usuario.isEditing = false; // Desativa o modo de edição
+        alert('Usuário editado com sucesso');
+        // Atualiza a lista de usuários com o novo valor
+        getAllUsersSys(); // Recarrega a lista de usuários
+      } else {
+        alert('Erro ao editar o usuário');
       }
+    } catch (error) {
+      console.error("Erro ao editar o usuário:", error);
+      alert('Erro ao tentar editar o usuário');
     }
+  };
 
-    getUsuarios();
 
-    console.log(userStore().user);
-    this.getAllUsers();
-  }
-};
+  const removeUser = async (usuario: any) => {
+    try {
+      const response = await apiClient.delete(`/usersys/${usuario.id}`);
+      if (response.status === 204) {
+        usuarios.value = usuarios.value.filter(u => u.id !== usuario.id); // Remove o usuário da lista local
+        alert('Usuário removido com sucesso');
+      } else {
+        alert('Erro ao tentar remover o usuário');
+      }
+    } catch (error) {
+      console.error("Erro ao remover o usuário:", error);
+      alert('Erro ao tentar remover o usuário');
+    }
+  };
+
+
+
+  onMounted(() => {
+    getAllUsersSys();
+
+    const getUsuarios = async() =>{
+        try{
+          const allUsers = await registros.getAllUsers();
+          console.log("USUARIOS:", allUsers);
+        }catch(error){
+          console.error("Erro:", error);
+        }
+      }
+
+      getUsuarios();
+
+      console.log(userStore().user);
+      getAllUsersSys();
+
+  });
+
 </script>
 
 <style scoped>
-/* Estilos para centralizar o retângulo */
+
 .container {
   display: flex;
   justify-content: center;
-  height: 89%;
+  height: 100%;
   width: 100%;
   position: absolute;
   z-index: 10000;
-  top: 0vh;
-  align-items: center;
+  top: 0;
+  align-items: flex-start;
+}
+
+.box {
+  display: flex; /* Usando flexbox para dividir em colunas */
+  width: 75%;
+  height: 52%;
+  border-radius: 20px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  position: relative;
+  top: 28px;
 }
 
 .containerDark {
-  background: #0a0012e3;
-  color:white
+  background-color: #5c0ea3;
 }
 
 .containerLight {
-  background-color: #f7f7f7cd;
+  background-color: #fff;
 }
 
-.fline1, .fline2{
+.block0 {
+  display: flex;
+  width: 100%;
+}
+
+.block1 {
+  flex: 1 1 250px; /* Tornando o bloco dos botões flexível, com largura mínima de 250px */
+  padding-right: 20px; /* Espaço entre os botões e a tabela */
+}
+
+.block2 {
+  flex: 3; /* O bloco da tabela ocupa o restante do espaço */
+}
+
+.table-container {
+  width: 90%;
+  max-height: 450px; /* Altura máxima para a tabela */
+  overflow: auto; /* Permite rolagem quando o conteúdo ultrapassar o tamanho */
+  border-radius: 10px;
+  margin-top: 20px; /* Espaçamento acima da tabela */
+  margin-left: 30px;
+}
+
+.fline1, .fline2 {
   display: grid;
   grid-template-columns: 2fr 2fr;
   gap: 2rem;
@@ -213,74 +269,10 @@ export default {
   margin-top: 3%;
 }
 
-.usernameDark{
+.usernameDark {
   background-color: rgb(56, 56, 56);
   color: rgb(255, 255, 255);
   border: 1px solid rgb(41, 41, 41);
-}
-
-.box {
-  width: 75%;
-  height: 72%;
-  border-radius: 20px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  position: relative;
-  top: 28px;
-  box-shadow: 0 1px 2px rgba(60, 64, 67, 0.3), 0 2px 6px 2px rgba(60, 64, 67, 0.15) 
-}
-
-/* Título no canto superior esquerdo */
-.title {
-  font-size: 1.2em;
-  margin: 0;
-}
-
-.block0{
-  display: flex;
-  flex-direction: row;
-  width: 102%;
-  height: 98%;
-}
-
-.block1{
-  width: 20%;
-  margin-top: 2%;
-}
-
-.block2 {
-  display: flex;
-  flex-direction: column;
-  margin-left: 8%;
-  width: 67%;
-}
-
-.case1{
-  display: flex;
-  flex-direction: column;
-}
-
-.case2{
-  display: flex;
-  flex-direction: column;
-}
-
-.case3{
-  display: flex;
-  flex-direction: column;
-}
-
-.case4{
-  display: flex;
-  flex-direction: column;
-}
-
-.blockForm{
-  margin-bottom: 15px;
-}
-
-.tableDark{
-  border: 1px solid rgba(211, 210, 210, 0.703);
 }
 
 .sidebar-button {
@@ -292,9 +284,9 @@ export default {
   font-size: 14px;
   margin-top: 12%;
   display: block;
-  background-color: #35005d;
-  color: white;
   border-radius: 8px;
+  background-color: #5c0ea3;
+  color: white;
   cursor: pointer;
 }
 
@@ -302,63 +294,40 @@ export default {
   background-color: #5c0ea3;
 }
 
-label {
+table {
+  width: 80%;
+  border-collapse: collapse;
+  border: 1px solid rgba(0, 0, 0, 0.265);
+}
+
+label{
   margin-bottom: 5px;
 }
 
-input, select {
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid rgb(156, 156, 156);
+th {
+  background-color: #000;
+  border: 1px solid #fff;
+  padding: 8px;
+  text-align: left;
+  color: white;
 }
 
-table {
-        width: 100%;
-        border-collapse: collapse;
-        border: 1px solid rgba(0, 0, 0, 0.265);
-    }
-    th {
-        background-color: #000;
-        border: 1px solid #fff;
-        padding: 8px;
-        text-align: left;
-        color: white;
-    }
-    td {
-        padding: 8px;
-        text-align: left;
-    }
-
-#bClose {
-  margin-top: 105%;
+td {
+  padding: 8px;
+  text-align: left;
 }
 
-.fade-enter-active {
-  animation: fadeInUp 0.3s ease-out;
+/* Estilos específicos para o botão "Editar" */
+.edit-button {
+  width: auto; /* Ajusta automaticamente o tamanho do botão */
+  padding: 6px 12px; /* Ajuste do padding (menor que o padrão) */
+  font-size: 12px; /* Ajuste do tamanho da fonte */
+  background-color: #5c0ea3; /* Cor de fundo diferente */
 }
 
-.fade-leave-active {
-  animation: fadeOutDown 0.3s ease-in forwards;
-}
-@keyframes fadeInUp {
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-@keyframes fadeOutDown {
-  from {
-    opacity: 1;
-    transform: translateY(0);
-  }
-  to {
-    opacity: 0;
-    transform: translateY(20px);
-  }
+.edit-button:hover {
+  background-color: #5c0ea3; /* Cor de hover diferente */
 }
 </style>
+
+
