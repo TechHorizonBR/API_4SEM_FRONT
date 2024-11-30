cd8f48eb9911ee325df74c95ed234b9a68d061fb
AndreMeneses0103 <andremeneses0103@gmail.com>
2024-11-29 21:55:29 -0300

    All verifications in password change added

--- src/components/ShowUser.vue
+++ src/components/ShowUser.vue
@@ -4,74 +4,189 @@
             class="box"
             :class="{ containerDark: isDark, containerLight: !isDark }"
         >
-            <h1 class="title">User Information</h1>
-            <div class="block0">
-                <div class="block1">
-                    <button class="sidebar-button">Reset Password</button>
-                    <button class="sidebar-button" id="bClose">Close</button>
-                </div>
+            <template v-if="!isReset">
+                <h1 class="title">User Information</h1>
+                <div class="block0">
+                    <div class="block1">
+                        <button class="sidebar-button" @click="swapIsReset()">
+                            Reset Password
+                        </button>
+                        <button class="sidebar-button" id="bClose">
+                            Close
+                        </button>
+                    </div>
 
-                <div class="block2">
-                    <div class="blockForm">
-                        <div
-                            class="fline1"
-                            :class="{ blockDark: isDark, blockLight: !isDark }"
-                        >
-                            <div class="info-item">
-                                <label>Creation Date:</label>
-                                <span>{{
-                                    userStr.user.createdAt
-                                        ? userStr.user.createdAt
-                                        : "Date not available"
-                                }}</span>
+                    <div class="block2">
+                        <div class="blockForm">
+                            <div
+                                class="fline1"
+                                :class="{
+                                    blockDark: isDark,
+                                    blockLight: !isDark,
+                                }"
+                            >
+                                <div class="info-item">
+                                    <label>Creation Date:</label>
+                                    <span>{{
+                                        userStr.user.createdAt
+                                            ? userStr.user.createdAt
+                                            : "Date not available"
+                                    }}</span>
+                                </div>
+                                <div class="info-item">
+                                    <label>Created By:</label>
+                                    <span>{{
+                                        userStr.user.createdBy
+                                            ? userStr.user.createdBy
+                                            : "Information not available"
+                                    }}</span>
+                                </div>
                             </div>
-                            <div class="info-item">
-                                <label>Created By:</label>
-                                <span>{{
-                                    userStr.user.createdBy
-                                        ? userStr.user.createdBy
-                                        : "Information not available"
-                                }}</span>
-                            </div>
-                        </div>
 
-                        <div class="fline2">
-                            <div class="info-item">
-                                <label>ID:</label>
-                                <span>{{
-                                    userStr.user.id
-                                        ? userStr.user.id
-                                        : "ID not available"
-                                }}</span>
-                            </div>
-                            <div class="info-item">
-                                <label>Name:</label>
-                                <span>{{
-                                    userStr.user.name
-                                        ? userStr.user.name
-                                        : "Name not available"
-                                }}</span>
-                            </div>
-                            <div class="info-item">
-                                <label>Role:</label>
-                                <span>{{
-                                    userStr.user.role
-                                        ? userStr.user.role
-                                        : "Role not available"
-                                }}</span>
-                            </div>
-                            <div class="info-item">
-                                <label>Username:</label>
-                                <span>{{
-                                    userStr.user.username
-                                        ? userStr.user.username
-                                        : "Username not available"
-                                }}</span>
+                            <div class="fline2">
+                                <div class="info-item">
+                                    <label>ID:</label>
+                                    <span>{{
+                                        userStr.user.id
+                                            ? userStr.user.id
+                                            : "ID not available"
+                                    }}</span>
+                                </div>
+                                <div class="info-item">
+                                    <label>Name:</label>
+                                    <span>{{
+                                        userStr.user.name
+                                            ? userStr.user.name
+                                            : "Name not available"
+                                    }}</span>
+                                </div>
+                                <div class="info-item">
+                                    <label>Role:</label>
+                                    <span>{{
+                                        userStr.user.role
+                                            ? userStr.user.role
+                                            : "Role not available"
+                                    }}</span>
+                                </div>
+                                <div class="info-item">
+                                    <label>Username:</label>
+                                    <span>{{
+                                        userStr.user.username
+                                            ? userStr.user.username
+                                            : "Username not available"
+                                    }}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </template>
+            <template v-else>
+                <h1 class="title">Change Password</h1>
+                <div class="block0">
+                    <div class="block1">
+                        <button class="sidebar-button" @click="swapIsReset()">
+                            User information
+                        </button>
+                    </div>
+
+                    <div class="block2_pass">
+                        <div class="all_pass">
+                            <div class="pass_input">
+                                <span>Type your new password</span>
+                                <input
+                                    type="password"
+                                    v-model="password"
+                                    name="password"
+                                    id="inp_pass"
+                                    placeholder="New password"
+                                    @input="verifyStrong"
+                                />
+                            </div>
+                            <div class="pass_input">
+                                <span>Confirm your new password</span>
+                                <input
+                                    type="password"
+                                    v-model="confirmPassword"
+                                    name="confirm_password"
+                                    id="inp_confirm_pass"
+                                    placeholder="Confirm password"
+                                />
+                            </div>
+                            <span v-if="(password.length > 0) && (password !== confirmPassword)" style="color: red; font-size: small">Passwords aren't the same</span>
+                            <button
+                                class="pass-button"
+                                @click="changePassword()"
+                            >
+                                Change
+                            </button>
+                        </div>
+                        <div class="strong_rules">
+                            <span
+                                style="font-size: larger; margin-bottom: 10px"
+                            >
+                                Your password must have:
+                            </span>
+                            <div class="all_rules">
+                                <span
+                                    :class="
+                                        strongRules.minChar
+                                            ? 'icon-check'
+                                            : 'icon-error'
+                                    "
+                                >
+                                    Minimum 8 characters
+                                </span>
+                                <span
+                                    :class="
+                                        strongRules.maxChar
+                                            ? 'icon-error'
+                                            : 'icon-check'
+                                    "
+                                >
+                                    Maximum 20 characters
+                                </span>
+                                <span
+                                    :class="
+                                        strongRules.lowerCase
+                                            ? 'icon-check'
+                                            : 'icon-error'
+                                    "
+                                >
+                                    At least one lowercase letter
+                                </span>
+                                <span
+                                    :class="
+                                        strongRules.upperCase
+                                            ? 'icon-check'
+                                            : 'icon-error'
+                                    "
+                                >
+                                    At least one uppercase letter
+                                </span>
+                                <span
+                                    :class="
+                                        strongRules.number
+                                            ? 'icon-check'
+                                            : 'icon-error'
+                                    "
+                                >
+                                    At least one number
+                                </span>
+                                <span
+                                    :class="
+                                        strongRules.specialChar
+                                            ? 'icon-check'
+                                            : 'icon-error'
+                                    "
+                                >
+                                    At least one special character
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
 </template>
@@ -85,9 +200,55 @@ export default {
     data() {
         return {
             userStr: userStore(),
+            isReset: false,
+            password: "",
+            confirmPassword: "",
+            strongRules: {
+                minChar: false,
+                maxChar: false,
+                lowerCase: false,
+                upperCase: false,
+                number: false,
+                specialChar: false,
+            },
+            isStrong: false,
         };
     },
-    methods: {},
+    methods: {
+        swapIsReset() {
+            this.isReset = !this.isReset;
+        },
+        verifyStrong() {
+            const password = this.password;
+
+            this.strongRules.minChar = password.length >= 8;
+            this.strongRules.maxChar = password.length > 20;
+            this.strongRules.lowerCase = /[a-z]/.test(password);
+            this.strongRules.upperCase = /[A-Z]/.test(password);
+            this.strongRules.number = /[0-9]/.test(password);
+            this.strongRules.specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(
+                password
+            );
+
+            const rulesWithoutMaxChar = { ...this.strongRules, maxChar: true };
+            this.isStrong = Object.values(rulesWithoutMaxChar).every(
+                (rule) => rule === true
+            );
+        },
+        changePassword() {
+            if (this.password !== this.confirmPassword) {
+                alert("Passwords aren't the same");
+                return;
+            } 
+
+            if(!this.isStrong){
+                alert("There are rules not being followed.")
+                return;
+            }
+
+            //fazer requisicao
+        },
+    },
     mounted() {},
 };
 </script>
@@ -195,9 +356,19 @@ export default {
 
 .block2 {
     display: flex;
-    flex-direction: column;
-    margin-left: 8%;
-    width: 67%;
+    flex-direction: row;
+    margin-left: 100px;
+    width: 100%;
+    justify-content: space-around;
+}
+
+.block2_pass {
+    /* display: flex; */
+    flex-direction: row;
+    margin-left: 100px;
+    width: 90%;
+    justify-content: space-around;
+    align-items: center;
 }
 
 .case1 {
@@ -243,6 +414,21 @@ export default {
     cursor: pointer;
 }
 
+.pass-button {
+    width: 150px;
+    padding: 10px;
+    margin-bottom: 25%;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    margin-top: 12%;
+    display: block;
+    background-color: #35005d;
+    color: white;
+    border-radius: 8px;
+    cursor: pointer;
+}
+
 .sidebar-button:hover {
     background-color: #5c0ea3;
 }
@@ -253,6 +439,7 @@ label {
 
 input,
 select {
+    outline: none;
     padding: 10px;
     border-radius: 5px;
     border: 1px solid rgb(156, 156, 156);
@@ -277,6 +464,34 @@ td {
 
 #bClose {
     margin-top: 105%;
+}
+
+.pass_input {
+    display: flex;
+    flex-direction: column;
+    margin: 10px 0;
+    width: 300px;
+}
+
+.all_pass {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.strong_rules,
+.all_rules {
+    display: flex;
+    flex-direction: column;
+}
+
+.strong_rules {
+    width: 300px;
+    margin-bottom: 100px;
+}
+
+.all_rules {
+    margin-left: 20px;
 }
 
 .fade-enter-active {
@@ -306,5 +521,32 @@ td {
         opacity: 0;
         transform: translateY(20px);
     }
+}
+
+.all_rules span::before {
+    content: "";
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+.icon-check {
+    color: green;
+}
+
+.icon-error {
+    color: red;
+}
+/* Ícone de check */
+.icon-check::before {
+    background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5"/></svg>');
+}
+
+/* Ícone de erro */
+.icon-error::before {
+    background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/></svg>');
 }
 </style>
